d39c25c2a20fe3c91ca0a8744b1ca29c6e227e52
Raj Joshi <geeky2307@gmail.com>
2019-07-12 22:34:37 -0400

    updated API

--- motorAPI.py
+++ motorAPI.py
@@ -3,20 +3,16 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 
-class Motor:
-    
+class Motor: 
     gpio_pi_pins = {1:{'e':11,'f':13,'r':15}, 4:{'e':32,'f':26,'r':24}}
     
     def __init__(self, motor):
         self.flag = self.gpio_pi_pins[motor]
-        
         GPIO.setup(self.flag['e'],GPIO.OUT)
         GPIO.setup(self.flag['f'],GPIO.OUT)
         GPIO.setup(self.flag['r'],GPIO.OUT)
-
         self.PWM = GPIO.PWM(self.flag['e'], 50)
         self.PWM.start(0)
-        
         GPIO.output(self.flag['e'],GPIO.HIGH)
         GPIO.output(self.flag['f'],GPIO.LOW)
         GPIO.output(self.flag['r'],GPIO.LOW)
@@ -26,7 +22,7 @@ class Motor:
         GPIO.output(self.flag['f'],GPIO.HIGH)
         GPIO.output(self.flag['r'],GPIO.LOW)
 
-    def backward(self, speed):
+    def reverse(self, speed):
         self.PWM.ChangeDutyCycle(speed)
         GPIO.output(self.flag['f'],GPIO.LOW)
         GPIO.output(self.flag['r'],GPIO.HIGH)        
@@ -36,16 +32,36 @@ class Motor:
         GPIO.output(self.flag['f'],GPIO.LOW)
         GPIO.output(self.flag['r'],GPIO.LOW)
 
-    def right(self, left, right, speed):
-        left.stop()
-        right.forward(speed)
-
-    def left(self, left, right, speed):
-        left.forward(speed)
-        right.stop()
-
-    def speed(self):
-        self.PWM.ChangeDutyCycle(speed)
-
     def shutdown(self):
         GPIO.cleanup()
+
+class Drivetrain:
+    def __init__(self, *motors):
+        self.motor = []
+        for i in motors:
+            self.motor.append(i)
+
+    def forward(self,speed):
+        for i in range(len(self.motor)):
+            self.motor[i].forward(speed)
+
+    def reverse(self,speed):
+        for i in range(len(self.motor)):
+            self.motor[i].reverse(speed)
+
+    def stop(self):
+        for i in range(len(self.motor)):
+            self.motor[i].stop()
+
+    def right(self, speed): 
+        self.motor[0].stop()
+        self.motor[1].forward(speed)
+
+    def left(self, speed):
+        self.motor[0].forward(speed)
+        self.motor[1].stop()
+    
+    def shutdown(self):
+        for i in range(len(self.motor)):
+            self.motor[i].shutdown()
+        
--- remote_control.py
+++ remote_control.py
@@ -1,27 +1,19 @@
 import motorAPI
 import getch
 
-left = motorAPI.Motor(4)
-right = motorAPI.Motor(1)
+robot = motorAPI.Drivetrain(motorAPI.Motor(4),motorAPI.Motor(1))
 
 while(True):
 	char = getch.getch()
 	if(char == 'p'):
 		break
 	if(char == 'w'):
-		left.forward(100)
-		right.forward(100)
+		robot.forward(100)
 	if(char == 'a'):
-		#left.forward(50)
-		#right.stop()
-		motorAPI.Motor.left(left,right,50)
+		robot.left(100)
 	if(char == 's'):
-		left.backward(100)
-		right.backward(100)
+		robot.reverse(100)
 	if(char == 'd'):
-		#right.forward(50)
-		#left.stop()
-		motorAPI.Motor.left(left,right,50)
-
-left.shutdown()
-right.shutdown()
+		robot.right(100)
+	if(char == 'b'):
+		robot.stop()
