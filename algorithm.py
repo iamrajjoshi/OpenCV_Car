b1bf28276dda8c6c6badaf7b06ecd688c9eed694
Raj Joshi <geeky2307@gmail.com>
2019-07-15 09:00:00 -0400

    found hsv vlues

--- algorithm.py
+++ algorithm.py
@@ -1,11 +1,18 @@
 #import motorAPI
 import cv2 as cv
-import numpy as numpy
+import numpy as np
 
 #robot = motorAPI.Drivetrain(motorAPI.Motor(4),motorAPI.Motor(1))
 
 frame = cv.imread('good/image2.jpg')
+frame = cv.resize(frame,(820,616))
 hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+#cv.namedWindow("", cv.WINDOW_NORMAL)
 cv.imshow('Frame', hsv)
+cv.waitKey(0)
+l = np.array([ 92,   4, 179] )
+h = np.array([132,  44, 279])
+mask = cv.inRange(hsv,l, h)
+cv.imshow('Frame', mask)
 
 cv.waitKey(0)
